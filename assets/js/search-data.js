b0c6d13f5c4c75b9421978444833da20d4f337dd
nestor-rdgz <51377031+nestor-rdgz@users.noreply.github.com>
2026-01-24 10:20:33 +0000

    Deploying to gh-pages from @ nestor-rdgz/nestor-rdgz.github.io@fa1076c7199d7befeed6f12f132053bc619176e1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "Courses I teach / have taught.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
